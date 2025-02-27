ab59b0b8276cf9beaf167ad7c04ef03bea4fd70b
japostad <japostad@student.42barcelona.com>
2025-02-27 20:25:24 +0100

    Mod: Move the functions to libft

--- src/utils.c
+++ src/utils.c
@@ -12,13 +12,6 @@
 
 #include "ft_printf.h"
 
-static int	ft_toupper(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (c - 32);
-	else
-		return (c);
-}
 void	ft_str_toupper(char *str)
 {
 	while (*str)
@@ -59,7 +52,7 @@ char	*ft_utoa_base(unsigned long long n, unsigned int base_len)
 	char	*base;
 	size_t	num_len;
 
-	base = "0123456789abcdefghijklmnopqrstuvwxyz";
+	base = "0123456789abcdef";
 	num_len = ft_ulen_base(n, base_len);
 	num = (char *)ft_calloc((num_len + 1), sizeof (char));
 	if (!num)
@@ -99,61 +92,3 @@ int	ft_printf_putnbr(char format, char *to_write, int *len)
 	}
 	return (*len);
 }
-void	*ft_calloc(size_t count, size_t size)
-{
-	size_t			i;
-	unsigned char	*output;
-
-	i = 0;
-	output = malloc(count * size);
-	if (!output)
-		return (NULL);
-	while (i < count * size)
-		output[i++] = 0;
-	return (output);
-}
-
-/*
-static int	ft_numlen(int n)
-{
-	int	len;
-
-	len = 0;
-	if (n <= 0)
-		len++;
-	while (n)
-	{
-		n /= 10;
-		len++;
-	}
-	return (len);
-}
-char	*ft_itoa(int n)
-{
-	char	*num;
-	int		num_len;
-	int		neg_flag;
-
-	neg_flag = 0;
-	num_len = ft_numlen(n);
-	num = (char *)ft_calloc((num_len + 1), sizeof (char));
-	if (!num)
-		return (NULL);
-	if (n < 0)
-		neg_flag = 1;
-	while (num_len--)
-	{
-		if (neg_flag)
-		{
-			if (num_len == 0)
-				num[num_len] = '-';
-			else
-				num[num_len] = '0' - (n % 10);
-		}
-		else
-			num[num_len] = '0' + (n % 10);
-		n /= 10;
-	}
-	return (num);
-}
-*/
