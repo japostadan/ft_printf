cb99ec9c30019811924822ef74286eab06e56f6c
japostad <japostad@student.42barcelona.com>
2025-02-27 20:22:01 +0100

    Mod: Used the libft library

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -11,39 +11,40 @@
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#define FT_PRINTF_H
 
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdlib.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* ft_printf.c */
-int					ft_select_format(char const format, va_list ap, int *len);
-int					ft_printf_logic(char const *format, va_list ap, int *len);
-int					ft_printf(char const *format, ...);
+int ft_select_format(char const format, va_list ap, int *len);
+int ft_printf_logic(char const *format, va_list ap, int *len);
+int ft_printf(char const *format, ...);
 
 /* ft_printf_utils.c */
-unsigned int		ft_baselen(char format);
-size_t				ft_ulen_base(unsigned long long n, unsigned int base_len);
-char				*ft_utoa_base(unsigned long long n, unsigned int base_len);
-int					ft_printf_putnbr(char format, char *to_write, int *len);
-char				*ft_itoa(int n);
-void				*ft_calloc(size_t count, size_t size);
+unsigned int ft_baselen(char format);
+size_t ft_ulen_base(unsigned long long n, unsigned int base_len);
+char *ft_utoa_base(unsigned long long n, unsigned int base_len);
+int ft_printf_putnbr(char format, char *to_write, int *len);
+char *ft_itoa(int n);
+void *ft_calloc(size_t count, size_t size);
+int ft_toupper(int c);
 
 /* ft_printf_char.c */
-int					ft_printf_char(va_list ap, int *len);
+int ft_printf_char(va_list ap, int *len);
 /* ft_printf_string.c */
-int					ft_printf_string(va_list ap, int *len);
+int ft_printf_string(va_list ap, int *len);
 /* ft_printf_int.c */
-int					ft_printf_int(char const format, va_list ap, int *len);
+int ft_printf_int(char const format, va_list ap, int *len);
 /* ft_printf_pointer.c */
-int					ft_printf_pointer(char const format, va_list ap, int *len);
+int ft_printf_pointer(char const format, va_list ap, int *len);
 /* ft_printf_uint.c */
-int					ft_printf_uint(char const format, va_list ap, int *len);
+int ft_printf_uint(char const format, va_list ap, int *len);
 
 /* ft_printf_hexa.c */
-int					ft_printf_hexa(char const format, va_list ap, int *len);
+int ft_printf_hexa(char const format, va_list ap, int *len);
 
 /* ft_printf_percent.c */
-int					ft_printf_percent(char const format, int *len);
+int ft_printf_percent(char const format, int *len);
 #endif
