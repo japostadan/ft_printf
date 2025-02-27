81a03c18ecd0539ad4a8eef5836ab0b9fd796ab9
japostad <japostad@student.42barcelona.com>
2025-02-27 22:13:48 +0100

    Mod: norm fix

--- src/ft_printf_pointer.c
+++ src/ft_printf_pointer.c
@@ -6,7 +6,7 @@
 /*   By: japostad <japostad@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 19:52:46 by japostad          #+#    #+#             */
-/*   Updated: 2025/01/13 19:52:51 by japostad         ###   ########.fr       */
+/*   Updated: 2025/02/27 22:12:30 by japostad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ int	ft_printf_pointer(char const format, va_list ap, int *len)
 	char				*to_write;
 
 	p = va_arg(ap, unsigned long long);
-    if ((void *)p == NULL)
-    {
-        write(1, "(nil)", 5);
-        *len += 5;
-    }
-    else
-    {
-        to_write = ft_utoa_base(p, ft_baselen(format));
-        if (!to_write)
-        {
-            (*len = -1);
-            return (*len);
-        }
-        ft_printf_putnbr(format, to_write, len);
-        free(to_write);
-    }
+	if ((void *)p == NULL)
+	{
+		write(1, "(nil)", 5);
+		*len += 5;
+	}
+	else
+	{
+		to_write = ft_utoa_base(p, ft_baselen(format));
+		if (!to_write)
+		{
+			(*len = -1);
+			return (*len);
+		}
+		ft_printf_putnbr(format, to_write, len);
+		free(to_write);
+	}
 	return (*len);
 }
